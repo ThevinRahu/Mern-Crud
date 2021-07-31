e4d9e8075ba77a9d9c6a468299985c141fea6a2b
ThevinRahu <thevinvr@gmail.com>
2021-07-31 23:08:51 +0530

    33mins 1st

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 const app = express();
-
+const postRoutes = require('./routes/posts'); // import routes
+app.use(bodyParser.json()); //app middleware
+app.use(postRoutes); //route middleware
 const port = 8000;
-
+const db_url =
+  "mongodb+srv://thevinmern:thevinmern123@mernapp.unfbx.mongodb.net/Mernapp?retryWrites=true&w=majority";
+mongoose
+  .connect(db_url, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("DB Connected!");
+  })
+  .catch((err) => console.log("DB Connection error!"));
 app.listen(port, () => {
-    console.log(`App is running on ${port}`);
+  console.log(`App is running on ${port}`);
 });
